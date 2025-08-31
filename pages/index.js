18fd1866256b5abd83864d768894b0703f7b1438
jesselaffiliates-rgb <jessel.affiliates@gmail.com>
2025-08-31 22:36:48 +0000

    Add Next.js deps + working index page + lockfile

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Welcome to Jarvis Starter</h1>
-      <p>This is a minimal Next.js app scaffolded by Jarvis.</p>
-    </div>
-  );
+  return <h1>Hello from Jarvis Starter</h1>;
 }
