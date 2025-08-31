ab7a498f9daed765795e73c849d2f4a4bf85a016
jesselaffiliates-rgb <jessel.affiliates@gmail.com>
2025-08-31 17:39:06 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Welcome to Jarvis Starter</h1>
-      <p>This is a minimal Next.js app scaffolded by Jarvis.</p>
-    </div>
-  );
-}
